2c877e191fb153e7c08cbc6ae90bd8514d7ad79f
neeraj <contactdhawan@gmail.com>
2016-10-31 20:31:03 -0500

    adding PathVariable

--- src/main/java/sf/crom/controller/HelloController.java
+++ src/main/java/sf/crom/controller/HelloController.java
@@ -1,18 +1,28 @@
 package sf.crom.controller;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/greeting")
 public class HelloController {
 
-	@RequestMapping("/welcome")
-	public ModelAndView sayHello() {
+	@RequestMapping("/hello/{name}/{country}")
+	public ModelAndView sayHello(@PathVariable("name") String name, @PathVariable("country") String country) {
 		
 		ModelAndView mav = new ModelAndView("HelloPage");
-		mav.addObject("welcomeMessage", "Papa Story sunao");
+		mav.addObject("welcomeMessage", "Hello World!!! "+name+" from "+country);
 		return mav;
 	}
 
+	@RequestMapping("/hi")
+	public ModelAndView sayHi(){
+		ModelAndView mav = new ModelAndView("HelloPage");
+		mav.addObject("welcomeMessage","Hi world!!!");
+		return mav;
+	}
 }

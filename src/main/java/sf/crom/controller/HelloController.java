cc10992d95d5da1a8482ab2cad91d7a068be8008
neeraj <contactdhawan@gmail.com>
2016-10-31 20:09:21 -0500

    using annotation

--- src/main/java/sf/crom/controller/HelloController.java
+++ src/main/java/sf/crom/controller/HelloController.java
@@ -1,23 +1,17 @@
 package sf.crom.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 @Controller
-public class HelloController extends AbstractController {
+public class HelloController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest req,
-			HttpServletResponse res) throws Exception {
-		System.out.println("In the controller...");
+	@RequestMapping("/welcome")
+	public ModelAndView sayHello() {
+		
 		ModelAndView mav = new ModelAndView("HelloPage");
-		mav.addObject("welcomeMessage", "Papa aana hai!!!!");
+		mav.addObject("welcomeMessage", "Papa Story sunao");
 		return mav;
 	}
 
